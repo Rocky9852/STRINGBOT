0aa29e9df6de4e6baa1cda6667082199f7cd8b33
Rocky9852 <abhisheklkr628@gmail.com>
2025-01-27 16:54:44 +0530

    Update userbot.py

--- RessoMusic/core/userbot.py
+++ RessoMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("untold_coder")
-                await self.one.join_chat("Youtube-_Offical_Network")
+                await self.one.join_chat("SaregamaSupport")
+                await self.one.join_chat("SaregamaSupport")
             except:
                 pass
             assistants.append(1)
